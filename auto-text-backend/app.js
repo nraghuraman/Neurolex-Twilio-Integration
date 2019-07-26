bd34dc9e3fe69c723c61a5eddd35882b35ad1cd5
Nikhil Raghuraman <nikhilvr@hotmail.com>
2019-07-25 21:32:57 -0400

    Remove unneeded code

--- auto-text-backend/app.js
+++ auto-text-backend/app.js
@@ -9,7 +9,6 @@ var monk = require('monk');
 var db = monk('localhost:27017/nodetest1');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -30,7 +29,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- auto-text-backend/routes/index.js
+++ auto-text-backend/routes/index.js
@@ -2,32 +2,8 @@ var express = require('express');
 var twilio = require('../twilio/send-sms');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/helloworld', function(req, res, next) {
-	res.render('helloworld', { title: 'Hello, world!' })
-});
-
-router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.find({},{},function(e,docs){
-    res.render('userlist', {
-      "userlist" : docs
-    });
-  });
-});
-
-router.get('/newuser', function(req, res) {
-  res.render('newuser', { title: 'Add New User' });
-});
-
 /* POST to Add User Service */
 router.post('/forminformation', async function(req, res) {
-
     // Set our internal DB variable
     var db = req.db;
 
@@ -38,7 +14,7 @@ router.post('/forminformation', async function(req, res) {
     var frequency = req.body.frequency;
     var frequencyUnits = req.body.frequencyUnits;
 
-    await twilio.sendAutomatedTexts(numbers, message, frequency, frequencyUnits);
+    twilio.sendAutomatedTexts(numbers, message, frequency, frequencyUnits);
 
     res.sendStatus(200);
 });
